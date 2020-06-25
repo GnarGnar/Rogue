3d8d11c025aa0879c058191f77dfae70e034ccc0
Kieran <kieranburke89@gmail.com>
2020-06-24 18:43:47 -0700

    Added item list and usage

--- engine.py
+++ engine.py
@@ -1,9 +1,9 @@
-import tcod, math
+import tcod, math, textwrap
 
 SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
 MAP_WIDTH = 80
-MAP_HEIGHT = 50
+MAP_HEIGHT = 43
 LIMITFPS = 20
 ROOM_MAX_SIZE = 10
 ROOM_MIN_SIZE = 6
@@ -12,10 +12,44 @@ FOV_ALGO = 0
 FOV_LIGHT_WALLS = True
 TORCH_RADIUS = 10
 MAX_ROOM_ENEMIES = 3
+MAX_ROOM_ITEMS = 2
+BAR_WIDTH = 20
+PANEL_HEIGHT = 7
+PANEL_Y = SCREEN_HEIGHT-PANEL_HEIGHT
+MSG_X = BAR_WIDTH + 2
+MSG_WIDTH = SCREEN_WIDTH - BAR_WIDTH - 2
+MSG_HEIGHT = PANEL_HEIGHT - 1
+INVENTORY_WIDTH = 50
+HEAL_AMOUNT = 4
+
+class Tile:
+    def __init__(self, blocked, blockSight = None):
+        self.blocked = blocked
+        self.explored = False
+        if blockSight is None:
+            blockSight = blocked
+        self.blockSight = blockSight
+
+
+class Rect:
+    def __init__(self, x, y, w, h):
+        self.x1 = x
+        self.y1 = y
+        self.x2 = x + w
+        self.y2 = y + h
+
+    def center(self):
+        centerX = (self.x1 + self.x2) / 2
+        centerY = (self.y1 + self.y2) / 2
+        return centerX, centerY
+
+    def intersect(self, other):
+        return self.x1 <= other.x2 and self.x2 >= other.x1 and self.y1 <= other.y1 and self.y2 >= other.y2
+
 
 class Object:
 
-    def __init__(self, x, y, char, name, color, blocks=False, fighter=None, ai=None):
+    def __init__(self, x, y, char, name, color, blocks=False, fighter=None, ai=None, item=None):
         self.x = x
         self.y = y
         self.char = char
@@ -28,6 +62,9 @@ class Object:
         self.ai = ai
         if self.ai:
             self.ai.owner = self
+        self.item = item
+        if self.item:
+            self.item.owner = self
 
 
     def move(self, dx, dy):
@@ -81,11 +118,15 @@ class Fighter:
     def attack(self, target):
         damage = self.power - target.fighter.defense
         if damage > 0:
-            print self.owner.name.capitalize() + ' attacks ' + target.name + ' for ' + str(damage) + ' hit points.'
+            messages(self.owner.name.capitalize() + ' attacks ' + target.name + ' for ' + str(damage) + ' hit points.')
             target.fighter.takeDamage(damage)
         else:
-            self.owner.name.capitalize() + ' attacks ' + target.name + ' but it has no effect!'
+            messages(self.owner.name.capitalize() + ' attacks ' + target.name + ' but it has no effect!')
 
+    def heal(self, healAmount):
+        self.hp += healAmount
+        if self.hp > self.maxHp:
+            self.hp = self.maxHp
 
 class BasicEnemy:
     def takeTurn(self):
@@ -98,29 +139,25 @@ class BasicEnemy:
                 enemy.fighter.attack(player)
 
 
-class Tile:
-    def __init__(self, blocked, blockSight = None):
-        self.blocked = blocked
-        self.explored = False
-        if blockSight is None:
-            blockSight = blocked
-        self.blockSight = blockSight
+class Item:
 
+    def __init__(self, useFunction=None):
+        self.useFunction = useFunction
 
-class Rect:
-    def __init__(self, x, y, w, h):
-        self.x1 = x
-        self.y1 = y
-        self.x2 = x + w
-        self.y2 = y + h
+    def pickup(self):
+        if len(inventory) >= 26:
+            messages('Your inventory is FULL!, cannot pick up' + self.owner.name + '.', tcod.red)
+        else:
+            inventory.append(self.owner)
+            objects.remove(self.owner)
+            messages('You picked up a ' + self.owner.name + '!', tcod.green)
 
-    def center(self):
-        centerX = (self.x1 + self.x2) / 2
-        centerY = (self.y1 + self.y2) / 2
-        return centerX, centerY
-
-    def intersect(self, other):
-        return self.x1 <= other.x2 and self.x2 >= other.x1 and self.y1 <= other.y1 and self.y2 >= other.y2
+    def use(self):
+        if self.useFunction is None:
+            messages('The ' + self.owner.name + ' cannot be used.')
+        else:
+            if self.useFunction() != 'cancelled':
+                inventory.remove(self.owner)
 
 
 def isBlocked(x, y):
@@ -169,9 +206,8 @@ def playerMoveOrAttack(dx, dy):
 
 
 def handleKeys():
-    global fovRecompute
+    global fovRecompute, key
     #options
-    key = tcod.console_check_for_keypress(True)
     if key.vk == tcod.KEY_ENTER and key.lalt:
         tcod.console_set_fullscreen(not tcod.console_is_fullscreen())
     elif key.vk == tcod.KEY_ESCAPE:
@@ -179,15 +215,25 @@ def handleKeys():
 
     #movement
     if gameState == 'playing':
-        if tcod.console_is_key_pressed(tcod.KEY_UP):
+        if key.vk == tcod.KEY_UP:
             playerMoveOrAttack(0, -1)
-        elif tcod.console_is_key_pressed(tcod.KEY_DOWN):
+        elif key.vk == tcod.KEY_DOWN:
             playerMoveOrAttack(0, 1)
-        elif tcod.console_is_key_pressed(tcod.KEY_LEFT):
+        elif key.vk == tcod.KEY_LEFT:
             playerMoveOrAttack(-1, 0)
-        elif tcod.console_is_key_pressed(tcod.KEY_RIGHT):
+        elif key.vk == tcod.KEY_RIGHT:
             playerMoveOrAttack(1, 0)
         else:
+            keyChar = chr(key.c)
+            if keyChar == 'g':
+                for object in objects:
+                    if object.x == player.x and object.y == player.y and object.item:
+                        object.item.pickup()
+                        break
+            if keyChar == 'i':
+                chosenItem = inventoryMenu('Press the key next to an item to use it, or any other to cancel. \n')
+                if chosenItem is not None:
+                    chosenItem.use()
             return 'didnt-take-turn'
 
 
@@ -257,16 +303,49 @@ def renderAll():
     player.draw()
 
     tcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
-    tcod.console_set_default_foreground(con, tcod.white)
-    tcod.console_print_ex(con, 1, SCREEN_HEIGHT - 2, tcod.BKGND_NONE, tcod.LEFT, 'HP: ' + str(player.fighter.hp) + '/' + str(player.fighter.maxHp))
+    tcod.console_set_default_background(panel, tcod.black)
+    tcod.console_clear(panel)
+    tcod.console_set_default_foreground(panel, tcod.light_gray)
+    tcod.console_print_ex(panel, 1, 0, tcod.BKGND_NONE, tcod.LEFT, getNamesUnderMouse())
 
+#messageLog
+    y = 1
+    for (line, color) in gameMSGs:
+        tcod.console_set_default_foreground(panel, color)
+        tcod.console_print_ex(panel, MSG_X, y, tcod.BKGND_NONE,tcod.LEFT, line)
+        y += 1
+#health bar
+    renderBar(1, 1, BAR_WIDTH, 'HP', player.fighter.hp, player.fighter.maxHp, tcod.light_red, tcod.darker_red)
+    tcod.console_blit(panel, 0, 0, SCREEN_WIDTH, PANEL_HEIGHT, 0, 0, PANEL_Y)
+
+def renderBar(x, y, totalWidth, name, value, max, barColor, backColor):
+    barWidth = int(float(value) / max * totalWidth)
+    tcod.console_set_default_foreground(panel, backColor)
+    tcod.console_rect(panel, x, y, totalWidth, 1, False, tcod.BKGND_SCREEN)
+    tcod.console_set_default_background(panel, barColor)
+    if barWidth > 0:
+        tcod.console_rect(panel, x, y, barWidth, 1, False, tcod.BKGND_SCREEN)
+    tcod.console_set_default_foreground(panel, tcod.white)
+    tcod.console_print_ex(panel, x + totalWidth / 2, y, tcod.BKGND_NONE, tcod.CENTER, name + ': ' + str(value) + '/' + str(max))
 
 def place_objects(room):
-    numEnemies = tcod.random_get_int(0, 0, MAX_ROOM_ENEMIES)
 
+#item loop
+    numItems = tcod.random_get_int(0, 0, MAX_ROOM_ITEMS)
+    for i in range(numItems):
+        x = tcod.random_get_int(0, room.x1 + 1, room.x2 - 1)
+        y = tcod.random_get_int(0, room.y1 + 1, room.y2 - 1)
+        if not isBlocked(x, y):
+            itemComponent = Item(useFunction=castHeal)
+            item = Object(x, y, '!', 'healing potion', tcod.violet, item=itemComponent)
+            objects.append(item)
+            item.sendToBack()
+
+    numEnemies = tcod.random_get_int(0, 0, MAX_ROOM_ENEMIES)
+#enemy loop
     for i in range(numEnemies):
-        x = tcod.random_get_int(0, room.x1, room.x2)
-        y = tcod.random_get_int(0, room.y1, room.y2)
+        x = tcod.random_get_int(0, room.x1 + 1, room.x2 - 1)
+        y = tcod.random_get_int(0, room.y1 + 1, room.y2 - 1)
 
         if not isBlocked(x, y):
             if tcod.random_get_int(0, 0, 100,) < 80:
@@ -283,13 +362,13 @@ def place_objects(room):
 
 def playerDeath(player):
     global gameState
-    print 'YOU DIED!'
+    messages('YOU DIED!', tcod.darkest_red)
     gameState = 'dead'
     player.char = '%'
     player.color = tcod.darkest_red
 
 def enemyDeath(enemy):
-    print enemy.name.capitalize() + ' is dead!'
+    messages(enemy.name.capitalize() + ' is dead!', tcod.orange)
     enemy.char = '%'
     enemy.color = tcod.darker_red
     enemy.blocks = False
@@ -298,7 +377,64 @@ def enemyDeath(enemy):
     enemy.name = 'remains of ' + enemy.name
     enemy.sendToBack()
 
+def messages(newMessage, color=tcod.white):
+    newMessageLines = textwrap.wrap(newMessage, MSG_WIDTH)
+    for line in newMessageLines:
+        if len(gameMSGs) == MSG_HEIGHT:
+            del gameMSGs[0]
+        gameMSGs.append((line, color))
 
+def getNamesUnderMouse():
+    global mouse
+    (x, y) = (mouse.cx, mouse.cy)
+    names = [obj.name for obj in objects
+             if obj.x == x and obj.y == y and tcod.map_is_in_fov(fovMap, obj.x, obj.y)]
+    names = ', '.join(names)
+    return names.capitalize()
+
+#remove letter from empty inventory
+def menu(header, options, width):
+    if len(options) > 26: raise ValueError('Cannot have a menu with more than 26 options.')
+    headerHeight = tcod.console_get_height_rect(con, 0, 0, width, SCREEN_HEIGHT, header)
+    height = len(options) + headerHeight
+    window = tcod.console_new(width, height)
+    tcod.console_set_default_foreground(window, tcod.white)
+    tcod.console_print_rect_ex(window, 0, 0, width, height, tcod.BKGND_NONE, tcod.LEFT, header)
+    y = headerHeight
+    letterIndex = ord('a')
+    for optionText in options:
+        text = '(' + chr(letterIndex) + ') ' + optionText
+        tcod.console_print_ex(window, 0, y, tcod.BKGND_NONE, tcod.LEFT, text)
+        y += 1
+        letterIndex += 1
+    x = SCREEN_WIDTH / 2 - width / 2
+    y = SCREEN_HEIGHT /2 - height / 2
+    tcod.console_blit(window, 0, 0, width, height, 0, x, y, 1.0, 0.7)
+    tcod.console_flush()
+    key = tcod.console_wait_for_keypress(True)
+
+    index = key.c - ord('a')
+    if index >= 0 and index < len(options): return index
+    return None
+
+def inventoryMenu(header):
+    if len(inventory) == 0:
+        options = ['Inventory empty!']
+    else:
+        options = [item.name for item in inventory]
+    index = menu(header, options, INVENTORY_WIDTH)
+    if index is None or len(inventory) == 0:
+        return None
+    return inventory[index].item
+
+def castHeal():
+    if player.fighter.hp == player.fighter.maxHp:
+        messages('You are already at full health!', tcod.red)
+        return 'cancelled'
+    messages('Your wounds start to feel better', tcod.lightest_cyan)
+    player.fighter.heal(HEAL_AMOUNT)
+
+#setup console
 tcod.console_set_custom_font('arial10x10.png', tcod.FONT_TYPE_GREYSCALE | tcod.FONT_LAYOUT_TCOD)
 tcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'python/RogueGame', False)
 con = tcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
@@ -310,6 +446,7 @@ playerFighterComponent = Fighter(hp=123, defense=3, power=8, deathFunction=playe
 #characters
 player = Object(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, '@', 'player', tcod.turquoise, blocks=True, fighter=playerFighterComponent)
 objects = [player]
+inventory = []
 
 #map colors
 colorLightWall = tcod.Color(130,110,50)
@@ -317,11 +454,12 @@ colorLightGround = tcod.Color(200,1080,50)
 colorDarkWall = tcod.Color(0, 0, 100)
 colorDarkGround = tcod.Color(50, 50, 150)
 
+#turns
 gameState = 'playing'
 playerAction = None
-
 makeMap()
 
+#field of view
 fovMap = tcod.map_new(MAP_WIDTH, MAP_HEIGHT)
 for y in range(MAP_HEIGHT):
     for x in range(MAP_WIDTH):
@@ -329,7 +467,20 @@ for y in range(MAP_HEIGHT):
 
 fovRecompute = True
 
+#stat bars
+panel = tcod.console_new(SCREEN_WIDTH, PANEL_HEIGHT)
+
+#game messages
+gameMSGs = []
+messages('Welcome stranger! Prepare to perish in the Tombs of the Ancient Kings.')
+
+#inputs
+mouse = tcod.Mouse()
+key = tcod.Key()
+
+#main loop
 while not tcod.console_is_window_closed():
+    tcod.sys_check_for_event(tcod.EVENT_KEY_PRESS|tcod.EVENT_MOUSE_PRESS,key,mouse)
     renderAll()
 
     tcod.console_flush()
